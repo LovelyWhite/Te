4cbefb17c0568de566fa953ca4d1f089517f61bd
lw <icezxn@qq.com>
2019-07-16 02:13:22 +0800

    v3.4

--- app/src/main/java/com/test/te/MainActivity.java
+++ app/src/main/java/com/test/te/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
     private String preTag = null;
     RemoteRSV remoteRSV = new RemoteRSV();
-    RealTimeData realTimeData = new RealTimeData();
+   // RealTimeData realTimeData = new RealTimeData();
     RemoteControl remoteControl = new RemoteControl();
     AlarmRecord alarmRecord = new AlarmRecord();
     HistoricalCurve historicalCurve = new HistoricalCurve();
@@ -32,10 +32,10 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.realTimeData://实时数据
-                    showFragment(preTag, "realTimeData");
-                    preTag = "realTimeData";
-                    return true;
+//                case R.id.realTimeData://实时数据
+//                    showFragment(preTag, "realTimeData");
+//                    preTag = "realTimeData";
+//                    return true;
                 case R.id.RemoteControl://远程控制
                     showFragment(preTag, "remoteControl");
                     preTag = "remoteControl";
@@ -117,31 +117,31 @@ public class MainActivity extends AppCompatActivity {
     }
     void buildFragments() {
         getSupportFragmentManager().beginTransaction()
-                .add(R.id.frameLayout, realTimeData, "realTimeData")
+              //  .add(R.id.frameLayout, realTimeData, "realTimeData")
                 .add(R.id.frameLayout, remoteControl, "remoteControl")
                 .add(R.id.frameLayout, remoteRSV, "remoteRSV")
                 .add(R.id.frameLayout, alarmRecord, "alarmRecord")
                 .add(R.id.frameLayout, historicalCurve, "historicalCurve")
                 .commit();
         getSupportFragmentManager().beginTransaction()
-                .hide(remoteControl)
+               // .hide(remoteControl)
                 .hide(remoteRSV)
                 .hide(alarmRecord)
                 .hide(historicalCurve)
                 .commit();
-        preTag="realTimeData";
-        realTimeData.startFlush();
+        preTag="remoteControl";
+       // realTimeData.startFlush();
     }
 
     void showFragment(String preTag, String tag) {
-        if(tag.equals("realTimeData"))
+      //  if(tag.equals("realTimeData"))
         {
             //开始刷新
-            realTimeData.startFlush();
+      //      realTimeData.startFlush();
         }
-        else
+     //   else
         {
-            realTimeData.stopFlush();
+      //      realTimeData.stopFlush();
         }
         if (!preTag.equals(tag)) {
             getSupportFragmentManager().beginTransaction().hide(Objects.requireNonNull(getSupportFragmentManager().findFragmentByTag(preTag)))
--- app/src/main/java/com/test/te/RemoteRSV.java
+++ app/src/main/java/com/test/te/RemoteRSV.java
@@ -6,6 +6,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Message;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -15,30 +16,40 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 
 public class RemoteRSV extends Fragment {
-
+    InfoUtils infoUtils;
     private ListView cValueList;
     private Spinner selectData;
     private Button rsvAdd;
     public ValueListAdapter valueListAdapter;
     LayoutInflater inflater;
+    CheckBox repeat;
     View remoteView;
+    Thread t;
+    boolean exit = false;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_remote_rsv, container, false);
         this.inflater=inflater;
+        infoUtils = new InfoUtils();
         cValueList = view.findViewById(R.id.cValueList);
         selectData = view.findViewById(R.id.selectData);
         valueListAdapter = new ValueListAdapter(view.getContext(),this);
         cValueList.setAdapter(valueListAdapter);
         rsvAdd = view.findViewById(R.id.rsvAdd);
+        repeat = view.findViewById(R.id.repeat);
         remoteView = view;
         return  view;
     }
@@ -52,8 +63,15 @@ public class RemoteRSV extends Fragment {
         selectData.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
             public void onItemSelected(AdapterView<?> arg0, View arg1,
                                        int arg2, long arg3) {
-                Data.nowTable=arg2;
-                valueListAdapter.notifyDataSetChanged();
+                if(t!=null&&t.isAlive())
+                {
+                    Toast.makeText(getContext(),"请关闭或等待循环读取结束",Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Data.nowTable=arg2;
+                    valueListAdapter.notifyDataSetChanged();
+                }
             }
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
@@ -66,9 +84,81 @@ public class RemoteRSV extends Fragment {
                 startActivityForResult(intent,1);
             }
         });
+        repeat.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b)
+                {
+                    exit = false;
+                    t = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            while (!exit)
+                            {
+                                try {
+                                    int count = valueListAdapter.getCount();
+                                    for (int i = 0; i <count; i++) {
+                                        if(exit)
+                                        {
+                                            break;
+                                        }
+                                        String data = ">" + Data.devices.get(Data.cDevicePosition).getDeviceID() + "&" + "03" + Data.dataLists.get(Data.tableList.get(Data.nowTable)).get(i).getAddress() + "0001#";
+                                        if(data.contains("null"))
+                                        {
+                                        }
+                                        else
+                                        {
+                                            String result = infoUtils.sendData(data
+                                                    , Data.devices.get(Data.cDevicePosition).getSocket()
+                                                    ,Data.devices.get(Data.cDevicePosition).getPosition());
+                                            result = result==null?"":result;
+                                            if (result.contains("Drive No online")) {
+                                            } else if(!result.equals("")) {
+                                                String a = result.split("&")[1].substring(4, 8);
+                                                String b =Data.dataLists.get(Data.tableList.get(Data.nowTable)).get(i).getMinUnit();
+                                                b= b==null?"1":b;
+                                                double v = Integer.parseInt(a, 16) * Double.parseDouble(b);
+                                                if(!b.contains("."))
+                                                {
+                                                    Data.dataLists.get(Data.tableList.get(Data.nowTable)).get(i).setcValue("" + (int)v);
+                                                }
+                                                else
+                                                {
+                                                    Data.dataLists.get(Data.tableList.get(Data.nowTable)).get(i).setcValue("" + v);
+                                                }
+                                            }
+                                        }
+                                        Thread.sleep(2000);
+                                    }
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+
+                            }
+                        }
+                    });
+                    t.start();
+                }
+                else
+                {
+                    exit = true;
+                }
+            }
+        });
     }
     public void notifyDataSetChanged()
     {
         valueListAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onDestroy() {
+        if(t.isAlive())
+        {
+            exit = true;
+        }
+        super.onDestroy();
     }
 }
